c8c9ecfef091b0af1ddc1fc04d2c47c0523eeedd
Maksim <pyshkov.maksim@gmail.com>
2021-01-14 02:35:22 +0200

    fix: .bat file deleted

--- npmInstall.bat
+++
@@ -1,7 +0,0 @@
-call npm i forever
-call npm i koa
-call npm i koa-body
-call npm i koa-static
-call npm i --save-dev eslint
-call npm i --save-dev eslint-config-airbnb-base
-call npm i --save-dev eslint-plugin-import
